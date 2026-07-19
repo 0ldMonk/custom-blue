e1a048b60e14172249ef80418bf4b4a04a584401
pi <pi@artfed>
2026-07-18 21:39:20 -0700

    Add zsh-autosuggestions

--- files/system/etc/zsh-extra.zsh
+++ files/system/etc/zsh-extra.zsh
@@ -27,3 +27,8 @@ if command -v starship > /dev/null 2>&1
 then
 	eval "$(starship init zsh)"
 fi
+
+if [[ -f /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh ]]
+then
+	source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+fi
